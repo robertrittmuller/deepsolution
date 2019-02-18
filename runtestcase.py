f831e54c8a3c61d9b23766f1f88d63a302569432
Robert Rittmuller <robert@rittmuller.com>
2019-02-18 17:19:11 -0500

    Fixed multiple bugs

--- runtestcase.py
+++ runtestcase.py
@@ -4,6 +4,7 @@ import pathlib
 import os
 import os.path
 import shutil
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,6 +17,9 @@ args = sys.argv[1:]
 testDirectory = pathlib.Path(args[0])
 testedDirectory = pathlib.Path(args[1])
 downloadDirectory = pathlib.Path(args[2])
+
+# define name to use for folder structure later on
+testedFolderName = ''
 
 # define the pattern to use for finding files in the download directory
 currentPattern = "*.js"
@@ -31,6 +35,15 @@ try:
     )
 
     for currentTestCase in testDirectory.glob(currentPattern):
+
+        # create folder for this test case's results
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        currentFileName = currentTestCase.stem
+        rootFileName = os.path.splitext(currentFileName)[0]
+        testedFolderName = rootFileName + '_' + timestr
+        resultsDirectory = testedDirectory.joinpath(str(testedFolderName))
+        resultsDirectory.mkdir()
+
         # Upload the test case
         fileinput = browser.find_element_by_id('filePicker')
         fileinput.send_keys(str(currentTestCase))
@@ -66,59 +79,62 @@ try:
         submit_button.click()
 
         time.sleep(5)
+
+        # start processing the test case outputs -------------------------------------------------------------------
+        try:
+            element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.ID, "filePicker"))
+            )
+            for currentFile in downloadDirectory.glob(currentPattern):  
+
+                # Upload the file to test
+                fileinput = browser.find_element_by_id('filePicker')
+                fileinput.send_keys(str(currentFile))
+
+                time.sleep(5)
+
+                # click first submit button
+                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+                submit_button.click()
+
+                time.sleep(5)
+
+                # click second submit button
+                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+                submit_button.click()
+
+                time.sleep(5)
+
+                # start the evaluation run
+                eval_button = browser.find_elements_by_xpath('//*[@id="evalButton"]')[0]
+                eval_button.click()
+
+                # wait until the evaluation ends
+                element = WebDriverWait(browser, 420).until(
+                    EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/p/b"))
+                )
+
+                # grab the score
+                element=browser.find_element_by_xpath('/html/body/div[3]/p/b')
+                ext = '_' + element.text[0:5] + '.js'
+                new_file_name = str(currentFile).replace('.js', str(ext))
+                os.rename(currentFile, new_file_name)
+                shutil.move(new_file_name, resultsDirectory)
+                print('Processed - ' + str(currentFile) + ' with a score of ' + element.text[0:5])
+
+                # click last submit button
+                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+                submit_button.click()
+
+                time.sleep(5)
+
+        finally:
+            print('Evaluation Run Completed!')
+
 finally:
     print('Test case runs completed!')
-
-# start processing the test case outputs -------------------------------------------------------------------
-try:
-    element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "filePicker"))
-    )
-    for currentFile in downloadDirectory.glob(currentPattern):  
-
-        # Upload the file to test
-        fileinput = browser.find_element_by_id('filePicker')
-        fileinput.send_keys(str(currentFile))
-
-        time.sleep(5)
-
-        # click first submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
-
-        time.sleep(5)
-
-        # click second submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
-
-        time.sleep(5)
-
-         # start the evaluation run
-        eval_button = browser.find_elements_by_xpath('//*[@id="evalButton"]')[0]
-        eval_button.click()
-
-        # wait until the evaluation ends
-        element = WebDriverWait(browser, 420).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/p/b"))
-        )
-
-        # grab the score
-        element=browser.find_element_by_xpath('/html/body/div[3]/p/b')
-        ext = '_' + element.text[0:5] + '.js'
-        new_file_name = str(currentFile).replace('.js', str(ext))
-        os.rename(currentFile, new_file_name)
-        shutil.move(new_file_name, testedDirectory)
-        print('Processed - ' + str(currentFile) + ' with a score of ' + element.text[0:5])
-
-         # click last submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
-
-        time.sleep(5)
-
-        
-finally:
     browser.quit()
+
+
 
 
