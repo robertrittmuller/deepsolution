0c563baa8a5cc9be6c421265d0d3b95e93ae7772
Robert Rittmuller <robert@rittmuller.com>
2019-02-23 22:14:35 -0500

    Possible fix for the long running crash

--- runtestcase.py
+++ runtestcase.py
@@ -9,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 # get a list of files to process using the passed path
 args = sys.argv[1:]
@@ -99,9 +100,13 @@ try:
         eval_button.click()
 
         # wait until the test case training ends
-        element = WebDriverWait(browser, 14420).until(
-            EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]"))
-        )
+        while True:
+            try:
+                popup = browser.find_element_by_xpath("/html/body/div[3]")
+                if popup.is_displayed():
+                   break
+            except NoSuchElementException:
+                continue
 
         time.sleep(5)
 
