746be8ab63cad41f67bc8130876f683140c30f78
Robert Rittmuller <robert@rittmuller.com>
2019-02-26 22:21:58 -0500

    Refactored to clean up messy code

--- runtestcase.py
+++ runtestcase.py
@@ -19,167 +19,175 @@ testDirectory = pathlib.Path(args[0])
 testedDirectory = pathlib.Path(args[1])
 downloadDirectory = pathlib.Path(args[2])
 
-# define name to use for folder structure later on
+# define global names to use for folder structure later on
 testedFolderName = ''
+resultsDirectory = ''
 
 # define the pattern to use for finding files in the download directory
 currentPattern = "*.js"
 
-# setup browser 
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.add_experimental_option("prefs", {
-  "download.prompt_for_download": False,
-  "download.directory_upgrade": True,
-  "safebrowsing.enabled": True,
-  "profile.default_content_setting_values.automatic_downloads": 1
-})
-chromeOptions.add_argument(f'user-agent={user_agent}')
-chromeOptions.add_argument('headless')
+def startBrowser():
+    # setup the browser instance and return it
+    # setup browser 
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_experimental_option("prefs", {
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+        "profile.default_content_setting_values.automatic_downloads": 1
+    })
+    chromeOptions.add_argument(f'user-agent={user_agent}')
+    chromeOptions.add_argument('headless')
 
-# run test cases -------------------------------------------------------------------------------------------
-try:
-# Start running test cases
-    for currentTestCase in testDirectory.glob(currentPattern):
-        # Load up the browser each time to minimize memory leak issues
-        print('Starting test case...')
-        browser=webdriver.Chrome(options=chromeOptions)
-        browser.implicitly_wait(0)
+    # create new browser instance
+    browser=webdriver.Chrome(options=chromeOptions)
+    browser.implicitly_wait(0)
 
-        # Just in case we are running in headless mode set up file downloads
-        browser.command_executor._commands["send_command"] = (
-            "POST",
-            '/session/$sessionId/chromium/send_command'
-        )
-        params = {
-            'cmd': 'Page.setDownloadBehavior',
-            'params': {
-                'behavior': 'allow',
-                'downloadPath': str(downloadDirectory)
-            }
+    # Just in case we are running in headless mode set up file downloads
+    browser.command_executor._commands["send_command"] = (
+        "POST",
+        '/session/$sessionId/chromium/send_command'
+    )
+    params = {
+        'cmd': 'Page.setDownloadBehavior',
+        'params': {
+            'behavior': 'allow',
+            'downloadPath': str(downloadDirectory)
         }
-        browser.execute("send_command", params)
+    }
+    browser.execute("send_command", params)
 
-        time.sleep(5)
+    return browser
 
-        # Load up DeepTraffic
-        browser.get('https://selfdrivingcars.mit.edu/deeptraffic/')
+def runTestCase(currentTestCase):
+    # Load up the browser each time to minimize memory leak issues
+    print('Starting test case...')
 
-        element = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.ID, "filePicker"))
-        )
+    time.sleep(5)
 
-        # create folder for this test case's results
-        timestr = time.strftime("%Y%m%d-%H%M%S")
-        currentFileName = currentTestCase.stem
-        rootFileName = os.path.splitext(currentFileName)[0]
-        testedFolderName = rootFileName + '_' + timestr
-        print('Working on test case ' + str(testedFolderName))
-        resultsDirectory = testedDirectory.joinpath(str(testedFolderName))
-        resultsDirectory.mkdir()
+    # Load up DeepTraffic
+    browser.get('https://selfdrivingcars.mit.edu/deeptraffic/')
 
-        # Upload the test case
-        fileinput = browser.find_element_by_id('filePicker')
-        fileinput.send_keys(str(currentTestCase))
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "filePicker"))
+    )
 
-        time.sleep(5)
+    # create folder for this test case's results
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    currentFileName = currentTestCase.stem
+    rootFileName = os.path.splitext(currentFileName)[0]
+    testedFolderName = rootFileName + '_' + timestr
+    print('Working on test case ' + str(testedFolderName))
+    resultsDirectory = testedDirectory.joinpath(str(testedFolderName))
+    resultsDirectory.mkdir()
 
-        # click first submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
+    # Upload the test case
+    fileinput = browser.find_element_by_id('filePicker')
+    fileinput.send_keys(str(currentTestCase))
 
-        time.sleep(5)
+    time.sleep(5)
 
-        # click second submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
+    # click first submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
 
-        time.sleep(5)
+    time.sleep(5)
 
-         # start the test case run (training)
-        eval_button = browser.find_elements_by_xpath('//*[@id="trainButton"]')[0]
-        eval_button.click()
+    # click second submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
 
-        # wait until the test case training ends
-        while True:
-            try:
-                popup = browser.find_element_by_xpath("/html/body/div[3]")
-                if popup.is_displayed():
-                   break
-            except NoSuchElementException:
-                continue
+    time.sleep(5)
 
-        time.sleep(5)
+        # start the test case run (training)
+    eval_button = browser.find_elements_by_xpath('//*[@id="trainButton"]')[0]
+    eval_button.click()
 
-        # click last submit button
-        submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-        submit_button.click()
-
-        time.sleep(5)
-
-        # start processing the test case outputs -------------------------------------------------------------------
+    # wait until the test case training ends
+    while True:
         try:
-            element = WebDriverWait(browser, 10).until(
-                EC.presence_of_element_located((By.ID, "filePicker"))
-            )
-            for currentFile in downloadDirectory.glob(currentPattern):  
+            popup = browser.find_element_by_xpath("/html/body/div[3]")
+            if popup.is_displayed():
+                break
+        except NoSuchElementException:
+            continue
 
-                # Upload the file to test
-                fileinput = browser.find_element_by_id('filePicker')
-                fileinput.send_keys(str(currentFile))
+    time.sleep(5)
 
-                time.sleep(5)
+    # click last submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
 
-                # click first submit button
-                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-                submit_button.click()
+    time.sleep(5)
 
-                time.sleep(5)
+    # clean up after everything is done!
+    browser.quit()
 
-                # click second submit button
-                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-                submit_button.click()
+def testResult(currentFile):
 
-                time.sleep(5)
-
-                # start the evaluation run
-                eval_button = browser.find_elements_by_xpath('//*[@id="evalButton"]')[0]
-                eval_button.click()
-
-                # wait until the evaluation ends
-                element = WebDriverWait(browser, 420).until(
-                    EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/p/b"))
-                )
-
-                # grab the score
-                element=browser.find_element_by_xpath('/html/body/div[3]/p/b')
-                score = element.text
-                score = score.replace("mph", "")
-                score = score.replace(" ", "")
-                if(float(score) >= 76):
-                    ext = '_' + score + '_FTW.js'
-                else:
-                     ext = '_' + score + '.js'
-
-                new_file_name = str(currentFile).replace('.js', str(ext))
-                os.rename(currentFile, new_file_name)
-                shutil.move(new_file_name, resultsDirectory)
-                print('Processed - ' + str(currentFile) + ' with a score of ' + element.text[0:5])
-
-                # click last submit button
-                submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
-                submit_button.click()
-
-                time.sleep(5)
-
-        finally:
-            print('Evaluation Run Completed!')
+    # make sure we see a filepicker before attempting anything...
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "filePicker"))
+    )
     
-        browser.quit()
+    # Upload the file to test
+    fileinput = browser.find_element_by_id('filePicker')
+    fileinput.send_keys(str(currentFile))
 
-finally:
-    print('Test case runs completed!')
+    time.sleep(5)
 
+    # click first submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
 
+    time.sleep(5)
 
+    # click second submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
 
+    time.sleep(5)
+
+    # start the evaluation run
+    eval_button = browser.find_elements_by_xpath('//*[@id="evalButton"]')[0]
+    eval_button.click()
+
+    # wait until the evaluation ends
+    element = WebDriverWait(browser, 420).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/p/b"))
+    )
+
+    # grab the score
+    element=browser.find_element_by_xpath('/html/body/div[3]/p/b')
+    score = element.text
+    score = score.replace("mph", "")
+    score = score.replace(" ", "")
+    if(float(score) >= 76):
+        ext = '_' + score + '_FTW.js'
+    else:
+            ext = '_' + score + '.js'
+
+    new_file_name = str(currentFile).replace('.js', str(ext))
+    os.rename(currentFile, new_file_name)
+    shutil.move(new_file_name, resultsDirectory)
+    print('Processed - ' + str(currentFile) + ' with a score of ' + element.text[0:5])
+
+    # click last submit button
+    submit_button = browser.find_elements_by_xpath('/html/body/div[3]/div[7]/div/button')[0]
+    submit_button.click()
+
+    time.sleep(5)
+
+# run test cases ----------------------------------------------------------------------------------------------
+browser = startBrowser()
+for currentTestCase in testDirectory.glob(currentPattern):
+    runTestCase(currentTestCase)
+    # start processing the test case outputs -------------------------------------------------------------------
+    browser.refresh
+    for currentFile in downloadDirectory.glob(currentPattern):  
+        testResult(currentFile)
+
+# Now our watch has ended
+browser.quit()
+print("Done processing test cases!")
